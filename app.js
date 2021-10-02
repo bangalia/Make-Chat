02549b000084d44aae0474a57fe4c3af35c22eb9
bangalia <anitabangali@yahoo.com>
2021-10-02 11:41:08 -0500

    Integrated Socket.io

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-//App.js
+//app.js
 const express = require('express');
 const app = express();
-//Socket.io has to use the http server
 const server = require('http').Server(app);
 
 //Express View Engine for Handlebars
 const exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+//Establish your public folder
+app.use('/public', express.static('public'))
+
+//Socket.io
+const io = require('socket.io')(server);
+io.on("connection", (socket) => {
+  console.log("🔌 New user connected! 🔌");
+})
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
@@ -16,3 +26,4 @@ app.get('/', (req, res) => {
 server.listen('3000', () => {
   console.log('Server listening on Port 3000');
 })
+
